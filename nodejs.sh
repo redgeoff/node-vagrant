8ec9199ea410f4a1240e2814f7d8c07bb531a744
Geoff Cox <redgeoff@gmail.com>
2017-05-18 20:30:18 -0700

    fix(node): remove unnecessary pkgs

--- nodejs.sh
+++ nodejs.sh
@@ -4,7 +4,7 @@
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 
 # Install node & build-essential (for make)
-apt-get install -y nodejs build-essential git
+apt-get install -y nodejs
 
 # Update npm
 npm install npm -g
