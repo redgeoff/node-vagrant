adae0a01ae2e20f13e38cfb65081e68b4d86ef49
Geoff Cox <redgeoff@gmail.com>
2017-07-13 20:18:43 -0700

    feat(node8)

--- nodejs.sh
+++ nodejs.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 # Get latest version of node
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 # Install node & build-essential (for make)
-apt-get install -y nodejs
+apt-get install -y nodejs build-essential
 
 # Update npm
 npm install npm -g
+
+# Required for node-gyp and leveldown. TODO: remove once this is no longer required
+apt-get install python
